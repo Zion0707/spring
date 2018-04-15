45dfa9f923f36e0135f1c90b8f91754a649bea2f
Zion0707 <Zion19920707@gmail.com>
2018-04-15 15:44:39 +0800

    修改

--- src/main/java/com/example/spring/HelloController.java
+++ src/main/java/com/example/spring/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController{
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello","/say"}, method = RequestMethod.GET)
     public String say(){
         return "Hello Spring boot!!!";
     }
