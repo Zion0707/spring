734eb5d6947bb780f6d7fe2bbd9419d5156dfd91
Zion0707 <Zion19920707@gmail.com>
2018-04-25 23:28:53 +0800

    change

--- src/main/java/com/example/spring/HelloController.java
+++ src/main/java/com/example/spring/HelloController.java
@@ -2,13 +2,14 @@ package com.example.spring;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController{
 
     @RequestMapping(value = {"/hello","/say"}, method = RequestMethod.GET)
-    public String say(){
-        return "Hello Spring boot!!!";
+    public String say(@RequestParam(value="id", required = false, defaultValue = "0") Integer id){
+        return "id=" + id;
     }
 }
